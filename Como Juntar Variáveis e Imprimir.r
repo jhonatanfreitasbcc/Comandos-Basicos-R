0ce24ce85d6cc13618b9e38c4ae00720c3df7c0d
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-12-12 09:02:01 -0300

    Como Juntar Variáveis e Imprimir.r

---
+++ Como Juntar Variáveis e Imprimir.r
@@ -0,0 +1,7 @@
+nome <- "Jhonatan"
+idade <- 25
+cidade <- "Campo Mourão"
+curso <- "Ciência da Computação"
+
+mensagem <- paste("Eu me chamo", nome, "tenho", idade, "anos e moro em", cidade, "e sou estudante de", curso)
+print(mensagem)
