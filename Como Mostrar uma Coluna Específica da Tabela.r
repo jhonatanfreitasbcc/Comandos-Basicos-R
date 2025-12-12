d76ffd689a5921d31528d8e7a3dba29dcf5b6347
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-12-12 18:37:36 -0300

    Como Mostrar uma Coluna Específica da Tabela.r

---
+++ Como Mostrar uma Coluna Específica da Tabela.r
@@ -0,0 +1,9 @@
+alunos <- data.frame(
+  Nome = c("JHONATAN", "FELIPE", "ANDRÉ"),
+  Idade = c(25, 20, 22),
+  Curso = c("Ciência da Computação", 
+             "Ciência da Computação", 
+             "Engenharia Eletrônica")
+)
+
+print(alunos$Curso)
