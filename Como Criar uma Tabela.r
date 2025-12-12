3a43d9e8fa521c4f0f6ec3276499ee8fef2a1db3
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-12-12 18:35:54 -0300

    Como Criar uma Tabela.r

---
+++ Como Criar uma Tabela.r
@@ -0,0 +1,7 @@
+alunos <- data.frame(
+  Nome = c("JHONATAN", "FELIPE", "ANDRÉ"),
+  Idade = c(25, 20, 22),
+  Curso = c("Ciência da Computação", 
+             "Ciência da Computação", 
+             "Engenharia Eletrônica")
+)
